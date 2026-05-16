4431e8f45a209a8b40bacbb5ad9f27d6d50586a6
Edwin Barragan <mbp@MacBook-Pro-de-Edxi.local>
2026-05-16 17:24:43 -0500

    correccion total transferencias cierre de caja 2

--- core/pos/views/cashClosing/views.py
+++ core/pos/views/cashClosing/views.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models import Sum, Q
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, FormView
@@ -191,11 +191,19 @@ class CashClosingCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateV
                     result=Sum('total')
                 )['result'] or Decimal('0.00')
 
-                cash_sales = sales_queryset.filter(
-                    paymentmethod='cash'
-                ).aggregate(
-                    result=Sum('total')
-                )['result'] or Decimal('0.00')
+                cash_sales = (
+                    sales_queryset.filter(
+                        paymentmethod='cash'
+                    ).aggregate(
+                        result=Sum('total')
+                    )['result'] or Decimal('0.00')
+                ) + (
+                    sales_queryset.filter(
+                        paymentmethod='mixto'
+                    ).aggregate(
+                        result=Sum('cash')
+                    )['result'] or Decimal('0.00')
+                )
 
                 credit_sales = sales_queryset.filter(
                     paymentmethod='creditCard'
@@ -335,11 +343,19 @@ class CashClosingCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateV
             result=Sum('total')
         )['result'] or Decimal('0.00')
 
-        cash_sales = sales_queryset.filter(
-            paymentmethod='cash'
-        ).aggregate(
-            result=Sum('total')
-        )['result'] or Decimal('0.00')
+        cash_sales = (
+            sales_queryset.filter(
+                paymentmethod='cash'
+            ).aggregate(
+                result=Sum('total')
+            )['result'] or Decimal('0.00')
+        ) + (
+            sales_queryset.filter(
+                paymentmethod='mixto'
+            ).aggregate(
+                result=Sum('cash')
+            )['result'] or Decimal('0.00')
+        )
 
         credit_sales = sales_queryset.filter(
             paymentmethod='creditCard'
@@ -353,11 +369,25 @@ class CashClosingCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateV
             result=Sum('total')
         )['result'] or Decimal('0.00')
 
-        transfer_sales = sales_queryset.filter(
-            paymentmethod='transfer'
-        ).aggregate(
-            result=Sum('total')
-        )['result'] or Decimal('0.00')
+        transfer_sales = (
+            sales_queryset.filter(
+                paymentmethod='transfer'
+            ).aggregate(
+                result=Sum('total')
+            )['result'] or Decimal('0.00')
+        ) + (
+            sales_queryset.filter(
+                paymentmethod='mixto'
+            ).aggregate(
+                result=Sum('nequi_value')
+            )['result'] or Decimal('0.00')
+        ) + (
+            sales_queryset.filter(
+                paymentmethod='mixto'
+            ).aggregate(
+                result=Sum('daviplata_value')
+            )['result'] or Decimal('0.00')
+        )
 
         expenses = expenses_queryset.aggregate(
             result=Sum('amount')
